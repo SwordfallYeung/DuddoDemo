59b46eefbe26953e99610889fa3bf92ffb08fcdd
GodYang <2506597416@qq.com>
2017-09-10 01:13:20 +0800

    提交dubbo学习demo

---
+++ Demoapi/src/main/java/test/DemoService.java
@@ -0,0 +1,5 @@
+package test;
+
+public interface DemoService {
+    String sayHello(String name);
+}
---
+++ Provider/src/main/java/test/DemoServiceImpl.java
@@ -0,0 +1,8 @@
+package test;
+
+public class DemoServiceImpl implements DemoService {
+    public String sayHello(String name) {
+        return "Hello1 "+name;
+    }
+
+}
